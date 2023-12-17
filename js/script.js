147346bf5f5c9da63a9fead06652d6476e1c1a61
Oleg Shahow <shahoww38@gmail.com>
2023-12-17 20:02:16 +0200

    бургер готов

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 "use strict";
+// для бургера прописать в html - id для -
+// nav , button , img-button
+
+const nav = document.querySelector("#nav");
+const navBtn = document.querySelector("#nav-btn");
+const navBtnImg = document.querySelector("#nav-btn-img");
+
+navBtn.onclick = () => {
+ if(nav.classList.toggle('open')){
+    navBtnImg.src="./nav/navClose.png";
+ }
+ else{
+    navBtnImg.src="./nav/NAV.png";
+ }
+}
